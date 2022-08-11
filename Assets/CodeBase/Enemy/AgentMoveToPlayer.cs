18260f50df3b6489ade0e3f380603df454b61817
Anishchenko Stanislav <anischenko_stas@mail.ru>
2022-08-11 19:21:41 +0500

    Created for enemy Lich NavMesh, animations

---
+++ Assets/CodeBase/Enemy/AgentMoveToPlayer.cs
@@ -0,0 +1,45 @@
+﻿using CodeBase.Infrastructure.Factory;
+using CodeBase.Infrastructure.Services;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace CodeBase.Enemy
+{
+    public class AgentMoveToPlayer : MonoBehaviour
+    {
+        [SerializeField] private NavMeshAgent Agent;
+
+        private const float MinimalDistance = 1;
+
+        private Transform _heroTransform;
+        private IGameFactory _gameFactory;
+
+        private void Start()
+        {
+            _gameFactory = AllServices.Container.Single<IGameFactory>();
+
+            if (_gameFactory.HeroGameObject != null)
+                InitializeHeroTransform();
+            else
+                _gameFactory.HeroCreated += HeroCreated;
+        }
+
+        private void Update()
+        {
+            if (Initialized() && HeroNotReached())
+                Agent.destination = _heroTransform.position;
+        }
+
+        private void HeroCreated() =>
+            InitializeHeroTransform();
+
+        private void InitializeHeroTransform() =>
+            _heroTransform = _gameFactory.HeroGameObject.transform;
+
+        private bool Initialized() =>
+            _heroTransform != null;
+
+        private bool HeroNotReached() =>
+            Vector3.Distance(Agent.transform.position, _heroTransform.position) >= MinimalDistance;
+    }
+}
