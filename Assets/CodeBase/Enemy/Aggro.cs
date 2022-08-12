7d215415b645cd3feeb5043f372f26da88455931
Anishchenko Stanislav <anischenko_stas@mail.ru>
2022-08-12 12:03:35 +0500

    Added aggression zone for Lich

---
+++ Assets/CodeBase/Enemy/Aggro.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Enemy
+{
+    public class Aggro : MonoBehaviour
+    {
+        [SerializeField] private TriggerObserver _triggerObserver;
+        [SerializeField] private AgentMoveToHero _follow;
+
+        private void Start()
+        {
+            _triggerObserver.TriggerEnter += TriggerEnter;
+            _triggerObserver.TriggerExit += TriggerExit;
+
+            SwitchFollowOff();
+        }
+
+        private void TriggerEnter(Collider obj) => SwitchFollowOn();
+
+        private void TriggerExit(Collider obj) => SwitchFollowOff();
+
+        private bool SwitchFollowOff() =>
+            _follow.enabled = false;
+
+        private bool SwitchFollowOn() =>
+            _follow.enabled = true;
+    }
+}
