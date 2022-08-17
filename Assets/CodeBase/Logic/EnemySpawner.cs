40c9ac822ea1cc785f971412a54958203b34ae7f
Anishchenko Stanislav <anischenko_stas@mail.ru>
2022-08-17 12:33:43 +0500

    Created MonsterTypeId, EnemySpawner and drawing it in Editor

---
+++ Assets/CodeBase/Logic/EnemySpawner.cs
@@ -0,0 +1,10 @@
+﻿using CodeBase.StaticData;
+using UnityEngine;
+
+namespace CodeBase.Logic
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        [SerializeField] private MonsterTypeId _monsterTypeId;
+    }
+}
